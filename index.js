3ef2f0bb782ca4cc8bf25b83ee3d3d17c798fdc5
Max <nocapulus@gmail.com>
2023-09-25 09:57:37 -0400

    creating products for a farm

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Product = require('./models/product'); // require the model
 const Farm = require('./models/farm');  // require the model
 
 
-mongoose.connect('mongodb://localhost:27017/farmStandMongoose', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/farmStandMongoose', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
     })
@@ -36,18 +36,36 @@ app.get('/farms/new', (req, res) => {
     res.render('farms/new')
 })
 
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id);
+    res.render('farms/show', { farm })
+})
+
 app.post('/farms', async (req, res) => {
     const farm = new Farm(req.body);
     await farm.save();
     res.redirect('/farms')
 })
 
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params;
+    res.render('products/new', { categories, id })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    // res.send(req.body) test if the data was sent first
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.send(farm)
+})
 
 //PRODUCT ROUTES________________________
-
-
-
-
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
